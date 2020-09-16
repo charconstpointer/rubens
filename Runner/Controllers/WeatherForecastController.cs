53f7e3e2b1c50b7e70ffa8bce6489076b25389d3
charconstpointer <attwistingnether@gmail.com>
2020-09-16 19:20:50 +0200

    IEventHandler.cs, MS DI

---
+++ Runner/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rubens.Components.Bus;
+
+namespace Runner.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly IBus _bus;
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IBus bus)
+        {
+            _logger = logger;
+            _bus = bus;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await _bus.Publish(new Event());
+            return Ok();
+        }
+    }
+}
